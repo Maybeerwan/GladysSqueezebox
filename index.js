8771a6dc62ae9af70c6e2ea8033badf5a185ae0c
Maybeerwan <maybemoi@gmail.com>
2018-01-10 22:06:30 +0100

    save

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ module.exports = function(sails) {
   gladys.on('ready', () => setup());
 
   return {
+    // Method called by Gladys to exec action on device
+    exec, // : exec
 
     // Method called by gladys to install/init module
-    install: install,
+    install, //: install,
 
     // Method called by gladys to notify
     // notify: notify,
 
     // Method called by Gladys when user click on the configure button
-    setup: setup,
-
-    // Method called by Gladys to exec action on device
-    exec: exec
+    setup //: setup,
 
   };
 
