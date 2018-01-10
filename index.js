108b266318a64b801749886f5c8d4936d3026605
Maybeerwan <maybemoi@gmail.com>
2018-01-10 21:35:52 +0100

    save

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = function(sails) {
 
   var setup = require('./lib/setup');
+  var install = require('./lib/install');
+  var exec = require('./lib/exec');
+  var notify = require('./lib/notify');
 
   // Event when Gladys start and is ready
   gladys.on('ready', () => setup());
@@ -8,16 +11,16 @@ module.exports = function(sails) {
   return {
 
     // Method called by gladys to install/init module
-    //install: require('./lib/install'),
+    install: install,
 
     // Method called by gladys to notify
-    //notify: require('./lib/notify'),
+    notify: notify,
 
     // Method called by Gladys when user click on the configure button
     setup: setup,
 
     // Method called by Gladys to exec action on device
-    //exec: require('./lib/exec'),
+    exec: exec,
 
   };
 
